867ea7ec2b28977e5ba958ccf76c9ebaabb60181
Paola Dolcemascolo <dolcemascop1@gmail.com>
2020-02-12 16:20:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,11 +1,13 @@
-def roll_call_dwarves(array)# code an argument here
+def roll_call_dwarves(array) # code an argument here
   # Your code here
   array.each_with_index{|item, index| 
   puts " #{index+1}. #{array[index]} "}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array) # code an argument here
   # Your code here
+  new_array = []
+  new_array = array.map{|word| "#{word.capitalize}!"}
 end
 
 def long_planeteer_calls# code an argument here
