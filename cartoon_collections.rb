7545c5048855906fbcca74ced24a2660c21291ba
Paola Dolcemascolo <dolcemascop1@gmail.com>
2020-02-12 16:07:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,7 @@
 def roll_call_dwarves(array)# code an argument here
   # Your code here
-  
+  array.each_with_index{|item, index| 
+  puts " #{index+1}. #{array[index]} "}
 end
 
 def summon_captain_planet# code an argument here
